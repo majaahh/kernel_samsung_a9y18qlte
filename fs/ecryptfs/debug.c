0b7fdabab123e9e2ae8dbfd4ae755aaa60ca56b4
Majaahh <maja.blaszczak@proton.me>
2025-09-11 23:38:05 +0200

    fs: Import SM-A920F changes from A920FXXS7CVL2
    Change-Id: I7edafc6626f403e58013d7c8128990576d6fb2c2
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- fs/ecryptfs/debug.c
+++ fs/ecryptfs/debug.c
@@ -95,6 +95,7 @@ void ecryptfs_dump_auth_tok(struct ecryptfs_auth_tok *auth_tok)
  *
  * Dump hexadecimal representation of char array
  */
+ #ifndef CONFIG_SDP
 void ecryptfs_dump_hex(char *data, int bytes)
 {
 	int i = 0;
@@ -118,7 +119,11 @@ void ecryptfs_dump_hex(char *data, int bytes)
 	if (add_newline)
 		printk("\n");
 }
-
+#else
+void ecryptfs_dump_hex(char *data, int bytes)
+{
+}
+#endif
 void ecryptfs_dump_salt_hex(char *data, int key_size,
 		const struct ecryptfs_crypt_stat *crypt_stat)
 {
@@ -139,11 +144,11 @@ void ecryptfs_dump_cipher(struct ecryptfs_crypt_stat *stat)
 	if (!stat)
 		return;
 
-	if (stat->cipher != NULL)
+	if (stat->cipher)
 		ecryptfs_printk(KERN_DEBUG,
 				"ecryptfs cipher is %s\n", stat->cipher);
 
-	if (stat->cipher_mode != NULL)
+	if (stat->cipher_mode)
 		ecryptfs_printk(KERN_DEBUG, "ecryptfs cipher mode is %s\n",
 				stat->cipher_mode);
 
--- fs/inode.c
+++ fs/inode.c
@@ -135,6 +135,7 @@ int inode_init_always(struct super_block *sb, struct inode *inode)
 	inode->i_sb = sb;
 	inode->i_blkbits = sb->s_blocksize_bits;
 	inode->i_flags = 0;
+	atomic64_set(&inode->i_sequence, 0);
 	atomic_set(&inode->i_count, 1);
 	inode->i_op = &empty_iops;
 	inode->i_fop = &no_open_fops;
@@ -177,6 +178,9 @@ int inode_init_always(struct super_block *sb, struct inode *inode)
 	mapping_set_gfp_mask(mapping, GFP_HIGHUSER_MOVABLE);
 	mapping->private_data = NULL;
 	mapping->writeback_index = 0;
+#ifdef CONFIG_SDP
+	mapping->userid = 0;
+#endif
 	inode->i_private = NULL;
 	inode->i_mapping = mapping;
 	INIT_HLIST_HEAD(&inode->i_dentry);	/* buggered by rcu freeing */
@@ -1784,6 +1788,7 @@ int file_update_time(struct file *file)
 	struct inode *inode = file_inode(file);
 	struct timespec now;
 	int sync_it = 0;
+	int need_sync = 0;
 	int ret;
 
 	/* First try to exhaust all avenues to not sync */
@@ -1797,7 +1802,19 @@ int file_update_time(struct file *file)
 	if (!timespec_equal(&inode->i_ctime, &now))
 		sync_it |= S_CTIME;
 
-	if (IS_I_VERSION(inode))
+	/* iversion impacts on "write" performance. This code just filter inodes
+	 * by presence in integrity cache (S_IMA flag, security/integrity/iint.c).
+	 * Because only FIVE uses iversion in Samsung Kernel this patch shouldn't
+	 * affect other code.
+	 * NOTICE: iversion code has been optimized in v4.17-rc4. So this patch should be
+	 * removed since v4.17-rc4
+	 */
+	#ifdef CONFIG_FIVE
+	need_sync = IS_I_VERSION(inode) && (inode->i_flags & S_IMA);
+	#else
+	need_sync = IS_I_VERSION(inode);
+	#endif
+	if (need_sync)
 		sync_it |= S_VERSION;
 
 	if (!sync_it)
