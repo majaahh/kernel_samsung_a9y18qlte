96f8e742c11743b71a73444effc4e11920db93a1
Majaahh <maja.blaszczak@proton.me>
2025-09-12 20:25:09 +0200

    build: Fetch submodules if anykernel.sh doesn't exist
    Change-Id: I7593298a632a458e296f98aae0c049c205c6d95f
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- do_build.sh
+++ do_build.sh
@@ -34,7 +34,10 @@ make -j$(nproc --all) ARCH=arm64 O=out SUBARCH=arm64 O=out \
     STRIP=${GCC_DIR}/bin/aarch64-linux-android-strip \
     CROSS_COMPILE=${GCC_DIR}/bin/aarch64-linux-android-
 
-if [ -d "ak3" ]; then
+if [[ -d "ak3" ]]; then
+    if [[ ! -f "ak3/anykernel.sh" ]]; then
+        git submodule update --init -f -q --checkout --recursive
+    fi
     mv "out/arch/arm64/boot/Image.gz-dtb" "ak3"
     cd "ak3"
     [ ! -d "$BUILDS_DIR" ] && mkdir -p "$BUILDS_DIR"
