f78a21eeabb82352ba204de41057235d85088c6b
Majaahh <maja.blaszczak@proton.me>
2025-12-05 22:17:43 +0100

    block: Import minimal changes from A920FXXS7CVL2
    Change-Id: Id162ab7ddca1453e211c36b6f1d5e5a7d0ea95fa
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- block/elevator.c
+++ block/elevator.c
@@ -538,7 +538,7 @@ void elv_bio_merged(struct request_queue *q, struct request *rq,
 #ifdef CONFIG_PM
 static void blk_pm_requeue_request(struct request *rq)
 {
-	if (rq->q->dev && !(rq->cmd_flags & REQ_PM))
+	if (rq->q->dev && !(rq->cmd_flags & REQ_PM) && rq->q->nr_pending)
 		rq->q->nr_pending--;
 }
 
@@ -736,6 +736,8 @@ void elv_completed_request(struct request_queue *q, struct request *rq)
 	 */
 	if (blk_account_rq(rq)) {
 		q->in_flight[rq_is_sync(rq)]--;
+		if (!queue_in_flight(q))
+			q->in_flight_time += ktime_us_delta(ktime_get(), q->in_flight_stamp);
 		if ((rq->cmd_flags & REQ_SORTED) &&
 		    e->type->ops.elevator_completed_req_fn)
 			e->type->ops.elevator_completed_req_fn(q, rq);
